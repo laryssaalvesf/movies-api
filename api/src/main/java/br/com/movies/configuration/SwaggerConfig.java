865fc568c5187d96debdb52a8234591ebcad2c5a
Laryssa Alves <laryssaalves2004@hotmail.com>
2020-01-17 06:41:52 -0300

    Adiciona informações no swagger

--- api/src/main/java/br/com/movies/configuration/SwaggerConfig.java
+++ api/src/main/java/br/com/movies/configuration/SwaggerConfig.java
@@ -2,8 +2,10 @@ package br.com.movies.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +20,15 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Movies API")
+                .description("API RESTful para leitura da lista de indicados e vencedores da categoria Pior Filme do Golden Raspberry Awards")
+                .version("1.0.0")
                 .build();
     }
 
